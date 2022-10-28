4f6926f72c6f1200fd77eb00d09fd08d9608170f
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-10-28 14:33:09 +0100

    Add class MyList

--- index.rb
+++ index.rb
@@ -1,2 +1,12 @@
-
-
+require_relative 'MyEnumerable'
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+  def each
+    (0...@list.length).each do |list|
+      yield @list[list]
+    end
+  end
+end
